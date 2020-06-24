e10a0dace1eff68f3e0e819ea9c57d5113ab616b
jlienard <jefferson.lienard@gmail.com>
2020-06-24 12:04:32 +0200

    adding router

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,20 @@
 <template>
   <div id="app">
     <Header />
-    <AddTodo v-on:add-todo="addTodo" />
-    <Todos v-bind:todos='todos' v-on:del-todo="deleteTodo" />
+    <router-view/>
   </div>
 </template>
 
 <script>
 import Header from './components/layout/Header';
-import Todos from './components/Todos';
-import AddTodo from './components/AddTodo';
-
 export default {
-  name: 'App',
+  name:"app",
   components: {
-    Header,
-    Todos,
-    AddTodo
-  },
-  data() {
-    return {
-      todos: [
-        {
-          id: 1,
-          title: "Todo One",
-          completed: false
-        },
-        {
-          id: 2,
-          title: "Todo Two",
-          completed: false
-        }
-      ]
-    }
-  },
-  methods: {
-    deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id);
-    },
-    addTodo(newTodo) {
-      this.todos = [...this.todos, newTodo];
-    }
+    Header
   }
 }
 </script>
+
 
 <style>
   * {
---
+++ src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>This is a project for testing Vue.js</h1>
+  </div>
+</template>
